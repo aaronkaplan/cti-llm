07c7fad5affa2981c8716eb7469caa2c15b206b2
aaronkaplan <aaron@lo-res.org>
2024-03-08 12:20:41 +0100

    move things out to common/

---
+++ common/web.py
@@ -0,0 +1,43 @@
+""" This module contains functions for fetching HTML from the web. """
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+
+def fetch_html_from_url_via_selenium(url: str) -> str:
+    """ Use Selenium to fetch HTML from the URL. """
+
+    # set up the Chrome options
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    # set up the Chrome service
+    service = Service("chromedriver")
+
+    # create the Chrome driver
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+
+    # fetch the URL
+    driver.get(url)
+
+    # wait for the page to load
+    try:
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+    except TimeoutException:
+        print("Timed out waiting for page to load")
+        return None
+
+    # get the HTML
+    html = driver.page_source
+
+    # close the driver
+    driver.quit()
+
+    return html
