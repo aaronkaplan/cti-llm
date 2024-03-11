b4a2d92db002931737c15a257b9ec1f881238155
priamai <paolo@priam.ai>
2024-03-11 23:56:12 +0000

    Added example with PDF parsing.

--- NER/poc/langchain_pdf_demo.py
+++ NER/poc/langchain_pdf_demo.py
@@ -1,17 +1,20 @@
 from langchain_community.document_loaders import PyPDFLoader
 from NER import cyner
+import glob
 
 # use only heuristics
 model_regex = cyner.CyNER(transformer_model=None,use_heuristic=True,flair_model=None,priority="H")
 
-loader = PyPDFLoader("../../datasets/MAR-10443863.r1.v1.CLEAR_.pdf")
-pages = loader.load_and_split()
+for pdf_report in glob.glob("../../datasets/*.pdf"):
+    print(pdf_report)
+    loader = PyPDFLoader(pdf_report)
+    pages = loader.load_and_split()
 
-for document in pages:
-    # this is a document
-    print(f"Parsing page {document.metadata['page']}")
+    for document in pages:
+        # this is a document
+        print(f"Parsing page {document.metadata['page']}")
 
-    entities = model_regex.get_entities(document.page_content)
-    print(f"Found {len(entities)} entities")
+        entities = model_regex.get_entities(document.page_content)
+        print(f"Found {len(entities)} entities")
 
 
