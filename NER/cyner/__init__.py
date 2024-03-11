ac22f5303c5e34077db421547625f83c0a4a26a3
priamai <paolo@priam.ai>
2024-03-11 22:50:37 +0000

    Focusing on heuristics.

--- NER/cyner/__init__.py
+++ NER/cyner/__init__.py
@@ -1,5 +1,5 @@
 from .cyner import CyNER
-from .transformers_ner import TransformersNER
+#from .transformers_ner import TransformersNER
 from .heuristics_ner import HeuristicsNER
-from .flair_ner import Flair
+#from .flair_ner import Flair
 from .spacy_ner import Spacy
