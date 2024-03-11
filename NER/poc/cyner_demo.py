7d6314d494cec7de95c1ab8a93eb0f1077c8db96
priamai <paolo@priam.ai>
2024-03-11 22:31:48 +0000

    Added a revamp of CyNer to centralize all the extractions.

---
+++ NER/poc/cyner_demo.py
@@ -0,0 +1,13 @@
+from NER import cyner
+
+# use only heuristics
+model_regex = cyner.CyNER(use_heuristic=True,flair_model=None)
+
+
+# Example text to test the matcher
+text = "The server at 192.168.1.1 responded with error code 404. Contact admin@example.com for details."
+
+entities = model_regex.get_entities(text)
+
+for e in entities:
+    print(e)
