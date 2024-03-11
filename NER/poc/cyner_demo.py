e4947135c60e4ad452c221502090c70e8a5993c0
priamai <paolo@priam.ai>
2024-03-11 22:55:41 +0000

    Added missing kor library.

--- NER/poc/cyner_demo.py
+++ NER/poc/cyner_demo.py
@@ -3,7 +3,6 @@ from NER import cyner
 # use only heuristics
 model_regex = cyner.CyNER(transformer_model=None,use_heuristic=True,flair_model=None,priority="H")
 
-
 # Example text to test the matcher
 text = "The server at 192.168.1.1 responded with error code 404. Contact admin@example.com for details."
 
