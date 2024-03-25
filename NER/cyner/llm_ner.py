3842d34127b7486bd7b0e12d680f61d1d09127d3
priamai <57333254+priamai@users.noreply.github.com>
2024-03-25 23:58:59 +0000

    Create llm_ner.py

---
+++ NER/cyner/llm_ner.py
@@ -0,0 +1,16 @@
+
+
+class LLMNER(EntityExtraction):
+    """
+    Entity extraction using LLM: Spacy-llm + KOR
+    """
+    def __init__(self, config):
+        super().__init__(config)
+        self.config = config
+        
+    def get_entities(self, text):
+        """ 
+        Add your magic here to return the entities
+        
+        """
+        pass
